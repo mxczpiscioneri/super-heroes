db47f81b40d642e5e0344c1294ee55d7cee6ca96
Matheus Piscioneri <matheus.piscioneri@luizalabs.com>
2019-05-29 22:13:15 -0300

    Test list Home

--- __tests__/pages/Home.test.js
+++ __tests__/pages/Home.test.js
@@ -1,20 +1,32 @@
 
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import Home from '../../src/pages/Home'
+import { charactesrMock } from '../../__mocks__/characters'
 
 const navigation = { navigate: jest.fn() }
 
-it('Home snapshot', () => {
-  const rendered = renderer.create(
-    <Home navigation={navigation} />
-  ).toJSON()
-  expect(rendered).toMatchSnapshot()
-})
+describe('Home', () => {
+  test('Home snapshot', () => {
+    const rendered = renderer.create(
+      <Home navigation={navigation} />
+    ).toJSON()
+    expect(rendered).toMatchSnapshot()
+  })
 
-it('Home snapshot', () => {
-  const rendered = renderer.create(
-    <Home navigation={navigation} />
-  ).toJSON()
-  expect(rendered).toMatchSnapshot()
+  test('Home list empty', () => {
+    const wrapper = shallow(<Home navigation={navigation} />)
+
+    expect(wrapper.find('FlatList').length).toEqual(1)
+    expect(wrapper.find('FlatList').props().data).toEqual([])
+  })
+
+  test('Home list', () => {
+    const wrapper = shallow(<Home navigation={navigation} />)
+    wrapper.setState({ characters: charactesrMock })
+
+    expect(wrapper.find('FlatList').length).toEqual(1)
+    expect(wrapper.find('FlatList').props().data).toEqual(charactesrMock)
+  })
 })
